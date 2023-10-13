ec8291dd60a64c6a46ec8e0c56c893eb2cb541ae
Edran0111 <82849327+Edran0111@users.noreply.github.com>
2023-10-13 14:46:34 +0200

    cors implementation

--- SimpleWalletBack/server.js
+++ SimpleWalletBack/server.js
@@ -2,16 +2,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000;
-
+const cors = require('cors');
 app.use(bodyParser.json());
+
+
+app.use(cors({
+    origin: (origin, callback) => {
+      const allowedOrigins = ['http://localhost:5173/'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+}));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-}
-);
+});
 
 
 app.listen(port, () => {
     console.log(`listening at http://localhost:${port}`);
 }
 );
+
+
+app.get
